05cf078dc6d6dd3c2dd033299ec097e60878db85
gabryelfrk <gabryelfranklinn@gmail.com>
2022-09-12 20:38:10 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-
+import BotaoDeNotificacao from "./components/BotaoDeNotificacao"
 
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <BotaoDeNotificacao />
+      <BotaoDeNotificacao />
+    </>
+
   )
 }
 
---
+++ frontend/src/components/BotaoDeNotificacao/index.tsx
@@ -0,0 +1,14 @@
+import icone from '../../assets/img/icone-notificacao.svg'
+import './styles.css'
+
+function BotaoDeNotificacao() {
+    return (
+        <>
+            <div className="dsmeta-red-btn">
+                <img src={icone} alt="Notificar" />
+            </div>
+        </>
+    )
+}
+
+export default BotaoDeNotificacao
