23460c5a5018480074d1609a7f9226bde589ecf2
gabryelfrk <gabryelfranklinn@gmail.com>
2022-09-15 18:45:02 -0300

    Consulta por data

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/VendaRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/VendaRepository.java
@@ -1,9 +1,17 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Venda;
 
 public interface VendaRepository extends JpaRepository<Venda, Long> {
-
+	
+	// JPQL
+	@Query("SELECT obj FROM Venda obj WHERE obj.data BETWEEN :min AND :max ORDER BY obj.total DESC")
+	Page<Venda> findVendas(LocalDate min, LocalDate max, Pageable pageable);
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/VendaService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/VendaService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Venda;
@@ -14,7 +18,13 @@ public class VendaService {
 	@Autowired
 	private VendaRepository repository;
 	
-	public List<Venda> findVendas() {
-		return repository.findAll();
+	public Page<Venda> findVendas(String dataMin, String dataMax, Pageable pageable) {
+		
+		LocalDate hoje = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = dataMin.equals("") ? hoje.minusDays(365) : LocalDate.parse(dataMin); // Converte de texto para localdate
+		LocalDate max = dataMax.equals("") ? hoje : LocalDate.parse(dataMax);
+		
+		return repository.findVendas(min, max, pageable);
 	}
 }
