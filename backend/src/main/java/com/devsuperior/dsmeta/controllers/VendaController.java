1fa8545504a585a884a9a530f50ee7f808c5a60f
gabryelfrk <gabryelfranklinn@gmail.com>
2022-09-16 19:45:45 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/VendaController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/VendaController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Venda;
+import com.devsuperior.dsmeta.services.SmsService;
 import com.devsuperior.dsmeta.services.VendaService;
 
 @RestController
@@ -18,11 +20,19 @@ public class VendaController {
 	@Autowired
 	private VendaService service;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping
 	public Page<Venda> findVendas(
 			@RequestParam(value = "dataMin", defaultValue = "") String dataMin, 
 			@RequestParam(value = "dataMax", defaultValue = "") String dataMax, 
 			Pageable pageable) {
 		return service.findVendas(dataMin, dataMax, pageable);
+	}
+	
+	@GetMapping("/{id}/notificacao")
+	public void notificacaoSms(@PathVariable Long id) {
+		smsService.enviarSms(id);
 	}
 }
---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.entities.Venda;
+import com.devsuperior.dsmeta.repositories.VendaRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.token}")
+	private String twilioToken;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	private VendaRepository vendaRepository;
+
+	public void enviarSms(Long vendaId) {
+		
+		Venda venda = vendaRepository.findById(vendaId).get();
+		
+		String data = venda.getData().getMonthValue() + "/" + venda.getData().getYear();
+		
+		String msg = "O vendedor " + venda.getNomeVendedor() + "foi destaque em " + data
+				   + " com um total de R$ " + String.format("%.2f", venda.getTotal());
+
+		Twilio.init(twilioSid, twilioToken);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
