92ea43476e37596dc256b227f638cadd739ec906
gabryelfrk <gabryelfranklinn@gmail.com>
2022-09-21 18:26:41 -0300

    Toast

--- frontend/src/components/BotaoDeNotificacao/index.tsx
+++ frontend/src/components/BotaoDeNotificacao/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icone from '../../assets/img/icone-notificacao.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -10,7 +11,7 @@ type Props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/vendas/${id}/notificacao`)
     .then(response => {
-        console.log("sucesso");
+        toast.info("SMS enviado com sucesso");
     });
 }
 
